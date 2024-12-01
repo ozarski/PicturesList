d034af284792494c98ec341e2fcc8bec0b4258d6
ozarskiw <ozarskiw@gmail.com>
2024-12-01 12:52:44 +0100

    Color scheme and other minor UI changes

--- app/src/main/java/com/example/lorempicsum/MainActivity.kt
+++ app/src/main/java/com/example/lorempicsum/MainActivity.kt
@@ -8,14 +8,23 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -24,6 +33,7 @@ import com.example.lorempicsum.ui.base.Screen
 import com.example.lorempicsum.ui.picturedetailsscreen.PictureDetailsScreen
 import com.example.lorempicsum.ui.picturelistscreen.PictureListScreen
 import com.example.lorempicsum.ui.theme.LoremPicsumTheme
+import com.example.lorempicsum.ui.theme.statusBarColor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -32,45 +42,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoremPicsumTheme {
-                Scaffold { paddingValues ->
-                    Surface(modifier = Modifier.padding(paddingValues)) {
-                        Column(
-                            verticalArrangement = Arrangement.Top,
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier.fillMaxSize()
-                        ) {
-                            val navController = rememberNavController()
-                            NavHost(
-                                navController = navController,
-                                startDestination = Screen.PictureListScreen.route,
-                                enterTransition = { EnterTransition.None },
-                                exitTransition = { ExitTransition.None },
+            Box(Modifier.fillMaxSize()) {
+                LoremPicsumTheme {
+                    Scaffold { paddingValues ->
+                        Surface(modifier = Modifier.padding(paddingValues)) {
+                            Column(
+                                verticalArrangement = Arrangement.Top,
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier.fillMaxSize()
                             ) {
-                                composable(Screen.PictureListScreen.route) {
-                                    PictureListScreen(
-                                        navController = navController,
-                                    )
-                                }
-                                composable(
-                                    Screen.PictureDetailScreen.route + "/{$PICTURE_ID_NAV_PARAM}",
-                                    enterTransition = {
-                                        slideInVertically(
-                                            initialOffsetY = { it },
-                                        )
-                                    },
-                                    exitTransition = {
-                                        slideOutVertically(
-                                            targetOffsetY = { it },
+                                val navController = rememberNavController()
+                                NavHost(
+                                    navController = navController,
+                                    startDestination = Screen.PictureListScreen.route,
+                                    enterTransition = { EnterTransition.None },
+                                    exitTransition = { ExitTransition.None },
+                                ) {
+                                    composable(Screen.PictureListScreen.route) {
+                                        PictureListScreen(
+                                            navController = navController,
                                         )
                                     }
-                                ) {
-                                    PictureDetailsScreen()
+                                    composable(
+                                        Screen.PictureDetailScreen.route + "/{$PICTURE_ID_NAV_PARAM}",
+                                        enterTransition = {
+                                            slideInVertically(
+                                                initialOffsetY = { it },
+                                            )
+                                        },
+                                        exitTransition = {
+                                            slideOutVertically(
+                                                targetOffsetY = { it },
+                                            )
+                                        }
+                                    ) {
+                                        PictureDetailsScreen()
+                                    }
                                 }
                             }
                         }
                     }
                 }
+
+                Spacer(
+                    modifier = Modifier
+                        .windowInsetsTopHeight(WindowInsets.statusBars)
+                        .fillMaxWidth()
+                        .align(Alignment.TopCenter)
+                        .background(
+                            statusBarColor
+                        )
+                )
             }
         }
     }
--- app/src/main/java/com/example/lorempicsum/ui/theme/Theme.kt
+++ app/src/main/java/com/example/lorempicsum/ui/theme/Theme.kt
@@ -1,23 +1,29 @@
 package com.example.lorempicsum.ui.theme
 
-import android.os.Build
+import android.app.Activity
+import android.provider.CalendarContract.Colors
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color.Black,
+    surface = Color.Black,
+    onSurface = Color.White,
+    surfaceContainer = Color(0xFF3A3F3D),
+    primaryContainer = Color(0xFF262626),
+    onPrimaryContainer = Color(0xFFA9CCBC),
 )
 
 private val LightColorScheme = lightColorScheme(
@@ -47,6 +53,7 @@ data class AppDimens(
     val cornerRadiusMedium: Dp = 10.dp,
     val cornerRadiusLarge: Dp = 16.dp,
 
+    val spacerExtraSmall: Dp = 4.dp,
     val spacerSmall: Dp = 8.dp,
     val spacerMedium: Dp = 16.dp,
     val spacerLarge: Dp = 24.dp,
@@ -62,7 +69,7 @@ data class AppDimens(
     val dividerThickness: Dp = 1.dp
 )
 
-val LocalAppDimens = staticCompositionLocalOf<AppDimens> {
+val LocalAppDimens = staticCompositionLocalOf {
     AppDimens()
 }
 
@@ -78,16 +85,18 @@ fun LoremPicsumTheme(
     ) {
         content()
     }
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = DarkColorScheme
+    //val colorScheme = when {
+    //    dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+    //        val context = LocalContext.current
+    //        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    //    }
 
-        darkTheme -> DarkColorScheme
+    //    darkTheme -> DarkColorScheme
 
-        else -> LightColorScheme
-    }
+    //    else -> LightColorScheme
+    //}
+
 
     MaterialTheme(
         colorScheme = colorScheme,
